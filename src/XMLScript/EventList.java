6c24cc2e0d769d3b67dd5ad4cc8ddb19f097ebb8
as5373u <as5373u@GM-KW304W-03.student.gre-ad.gre.ac.uk>
2019-04-09 21:15:10 +0100

    Final

--- src/XMLScript/EventList.java
+++ src/XMLScript/EventList.java
@@ -15,49 +15,28 @@ import java.util.List;
  */
 public class EventList {
     
-    // key is event type, value is list of listeners for that type
     HashMap<String, List<EventListener>> eventListeners;
 
-    /**
-     * default constructor
-     */
     public EventList() {
         eventListeners = new HashMap<>();
     }
 
-    /**
-     * Add a DashBoardEventListener
-     *
-     * @param type - the type of event listened for (e.g "speed")
-     * @param listener - reference to the listener object
-     */
     public void addListener(String type, EventListener listener) {
         List<EventListener> dbl = eventListeners.get(type);
-        if (dbl == null) { // if no listeners for this type already registered
-            dbl = new ArrayList<>(); // create a new list
+        if (dbl == null) {
+            dbl = new ArrayList<>();
         }
         dbl.add(listener); // add the listener to the list
         eventListeners.put(type, dbl); // update the map
     }
 
-    /**
-     * Remove a DashBoardEventListener
-     *
-     * @param type - the type of event listened for (e.g "speed")
-     * @param listener - reference to the listener object
-     */
     public void removeListener(String type, EventListener listener) {
         List<EventListener> dbl = eventListeners.get(type);
-        if (dbl != null) { // if there are any listeners for the specified event type
-            while (dbl.remove(listener)); // remove listener looping in case more than one
+        if (dbl != null) {
+            while (dbl.remove(listener));
         }
     }
 
-    /**
-     * Return a list of DashBoardEventListeners for a specified type of event
-     * @param type - the type of event for which listeners are required (e.g. "speed")
-     * @return - the list of listeners of the specified type
-     */
     public List<EventListener> getListeners(String type) {
         return eventListeners.get(type);
     }
--- src/XMLScript/GenerateXML.java
+++ src/XMLScript/GenerateXML.java
@@ -25,59 +25,30 @@ public class GenerateXML extends DefaultHandler{
     
     public final static String EVENT_TAG = "dashboard_event";
 
-    /**
-     * XML tag used to indicate the type of event
-     */
     public final static String TYPE_TAG = "type";
 
-    /**
-     * XML tag used to indicate the value of event
-     */
     public final static String VALUE_TAG = "value";
 
-    /**
-     * XML tag used to indicate the a time delay in the script
-     */
     public final static String DELAY_TAG = "delay";
 
-    /**
-     * How many milliseconds each unit in a delay element is to cause the script
-     * to pause for. To speed up the script processing, decrease the number. To
-     * slow the script processing, increase the number.
-     */
-    public final static int delayUnits = 100; // 1000 = 1 second
+    public final static int delayUnits = 100;
 
-    // current event being processed
     private dashboardEvent currentEvent = null;
-    private String currentTag = ""; // current tag being processed
+    private String currentTag = "";
 
-    // list of listeners registered to receive dashboard events
     private final EventList dashBoardListeners;
 
-    // the xml parser object
     private final XMLReader xmlReader;
 
-    /**
-     *
-     * @throws Exception - problem creating the parser
-     */
     public GenerateXML() throws Exception {
         dashBoardListeners = new EventList();
 
-        // The following code configures and creates an object known as a SAXParser which is capable of
-        // reading and interpreting an XML file.
         SAXParserFactory spf = SAXParserFactory.newInstance();
         spf.setNamespaceAware(true);
         SAXParser saxParser = spf.newSAXParser();
         xmlReader = saxParser.getXMLReader();
     }
 
-    /**
-     *
-     * @param filename - filename of the XML file to be processed
-     * @throws IOException - problem reading the file
-     * @throws SAXException - problem parsing the file
-     */
     public void processScriptFile(String filename) throws IOException, SAXException {
         // register the current object to receive callbacks when elements are encountered in the XML file
         xmlReader.setContentHandler(this);
@@ -86,17 +57,6 @@ public class GenerateXML extends DefaultHandler{
         xmlReader.parse(convertToFileURL(filename));
     }
 
-    /**
-     * startElement() is called by the parser whenever a start tag (tag =
-     * element) is encountered in the XML file. Store the tag's name and if it
- is an EVENT_tag create a new DashBoardEvent object that will be
- populated with data by the character() method
-     *
-     * @param namespaceURI
-     * @param localName - the name of the tag e.g. "dashboard_event"
-     * @param atts
-     * @throws SAXException - problem parsing the file
-     */
     @Override
     public void startElement(String namespaceURI,
             String localName,
@@ -111,29 +71,15 @@ public class GenerateXML extends DefaultHandler{
         }
     }
 
-    /**
-     * characters() is called by the parser when character content is to be
-     * processed. Note that we need to check what type of tag is currently being
-     * processed in order to know what to do with the characters. For instance
-     * if we are processing a delay tag we want to pause processing for the
-     * specified amount of time.
-     *
-     * @param ch - array holding the characters to be processed
-     * @param start - start position of current characters within the array
-     * @param length - number of characters to process
-     * @throws SAXException
-     */
     @Override
     public void characters(char ch[], int start, int length)
             throws SAXException {
-        // get the characters into a String and lose any unwanted whitespace
         String val = new String(ch, start, length).trim();
 
-        if (val.length() < 1) {  // is no characters to process (was all whitespace) just return
+        if (val.length() < 1) { 
             return;
         }
 
-        // process the characters based on what type of tag we are currently dealing with.
         switch (currentTag) {
             case TYPE_TAG:
                 currentEvent.setType(val);
@@ -145,30 +91,16 @@ public class GenerateXML extends DefaultHandler{
                 pause(Integer.parseInt(val));
                 break;
         }
+    
     }
-
-    /**
-     * endElement() is called by the parser when the end tag of an element is
-     * encountered. At that point know that we have finished processing that
-     * tag. The only situation we are interested in is when the end
-     * "dashboard_event" tag is found. At that [point we know that we have all
-     * the data for the event and can fire the event by creating the event
-     * object and passing it to all the listeners
-     *
-     * @param uri
-     * @param localName - the name of the tag e.g. "dashboard_event"
-     * @param qName
-     * @throws SAXException
-     */
+        
     @Override
     public void endElement(String uri, String localName, String qName)
             throws SAXException {
 
         if (localName.equals(EVENT_TAG)) {
-            // get all listeners
             List<EventListener> listeners = dashBoardListeners.getListeners(currentEvent.getType());
             if (listeners != null) {
-                // loop through the listeners passing the event object to them - this is "firing" the event
                 for (EventListener dbel : listeners) {
                     dbel.processDashBoardEvent(this, currentEvent);
                 }
@@ -177,11 +109,7 @@ public class GenerateXML extends DefaultHandler{
         }
         currentTag = "";
     }
-
-    /**
-     *
-     * @param delay - the length of the delay
-     */
+    
     private void pause(int delay) {
         try {
             Thread.sleep(delay * delayUnits);
@@ -189,37 +117,15 @@ public class GenerateXML extends DefaultHandler{
             Logger.getLogger(GenerateXML.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    /**
-     * registerDashBoardEventListener() is called by objects that want to be
-     * notified when a dashboard event occurs,
-     *
-     * @param type - type of the event listener is interested in (e.g. "speed")
-     * @param dbel - reference to the listener object
-     */
+    
     public void registerDashBoardEventListener(String type, EventListener dbel) {
         dashBoardListeners.addListener(type, dbel);
     }
 
-    /**
-     * removeDashBoardEventListener() is called by objects that not longer want
-     * to be notified when a dashboard event occurs,
-     *
-     * @param type - type of the event listener wishes to deregister for (e.g.
-     * "speed")
-     * @param dbel - reference to the listener object
-     */
     public void removeDashBoardEventListener(String type, EventListener dbel) {
         dashBoardListeners.removeListener(type, dbel);
     }
 
-    /**
-     * convertToFileURL() is a utility method just used if DashboardDemoMain is
-     * run in standalone mode to test the class
-     *
-     * @param filename - name of the xml file
-     * @return filename in URL format e.g "file:///c:/files/file.xml"
-     */
     private static String convertToFileURL(String filename) {
         String path = new File(filename).getAbsolutePath();
         if (File.separatorChar != '/') {
@@ -232,20 +138,13 @@ public class GenerateXML extends DefaultHandler{
         return "file:" + path;
     }
 
-    /**
-     * Output a help message to the user
-     */
+    
     private static void usage() {
         System.err.println("Usage:  DashboardEventGeneratorFromXML <file.xml>");
         System.err.println("       -usage or -help = this message");
         System.exit(1);
     }
 
-    /**
-     * main() method is only used if the class is run in standalone mode for testing purposes.
-     * @param args - last argument (args[length-1]) is the name of the xml file to process
-     * @throws Exception
-     */
     public static void main(String[] args) throws Exception {
         String filename = null;
 
@@ -271,8 +170,6 @@ public class GenerateXML extends DefaultHandler{
         };
         me.registerDashBoardEventListener("speed", dbel);
         me.processScriptFile(filename);
-//        me.removeDashBoardEventListener("speed", dbel);
-//        me.processScriptFile(filename);
     }
     
 }
